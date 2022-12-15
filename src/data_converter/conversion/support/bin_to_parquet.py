d4b5fa77eef4087993acb1b9ac4d179b5d473964
Ryan Oldford <ryan.oldford@gmail.com>
2022-12-14 16:25:58 -0800

    Basic conversion flow outlined

--- src/data_converter/conversion/support/bin_to_parquet.py
+++ src/data_converter/conversion/support/bin_to_parquet.py
@@ -5,4 +5,13 @@
 #   bit 1: Calib_energy saved
 #   bit 2: Energyshort saved
 #   bit 3: Waveform samples saved
-# calculate 
+# create struct/np_array for non-sample data
+# create 
+# calculate total bit size of these fields:
+#   board, channel, timestamp, energy, calib_energy, energyshort, flags, waveform code
+# read non-sample bits, decode to struct, add to list
+# read n_samples, decode
+# use n_samples to read/decode samples, add to list
+# convert to DataFrame every X records, add to list
+# concat DataFrames every N DataFrame conversions
+

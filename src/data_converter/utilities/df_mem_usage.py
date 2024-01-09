bf57f9433c4c02f8bf1b2dd0f10d2b48d20d8218
Ryan Oldford <ryan.oldford@berlinguettegroup.com>
2024-01-08 16:23:19 -0800

    Revert "Bugfix: no longer fails on large dataframe concat"
    This reverts commit 42e969aed79c1d2646357402d80baa2f014b9212.

--- src/data_converter/utilities/df_mem_usage.py
+++ src/data_converter/utilities/df_mem_usage.py
@@ -1,15 +1,14 @@
-from typing import Dict, Union
+from typing import Dict
 
 from pandas import DataFrame
-from modin.pandas import DataFrame as ModinDataFrame
 
 
-def get_df_total_mem_usage(df: Union[DataFrame, ModinDataFrame], index: bool = True) -> float:
+def get_df_total_mem_usage(df: DataFrame) -> float:
     # returns mem usage in MB
-    mem_usage = df.memory_usage(deep=True, index=index)
+    mem_usage = df.memory_usage(deep=True)
     return float(mem_usage.sum(skipna=True)) / (1024 * 1024)
 
 
-def get_df_dict_mem_usage(df_dict: Dict[str, DataFrame], index: bool = True) -> float:
-    df_usage = [get_df_total_mem_usage(df, index=index) for df in df_dict.values()]
+def get_df_dict_mem_usage(df_dict: Dict[str, DataFrame]) -> float:
+    df_usage = [get_df_total_mem_usage(df) for df in df_dict.values()]
     return sum(df_usage)
--- src/data_converter/utilities/save_to_parquet.py
+++ src/data_converter/utilities/save_to_parquet.py
@@ -1,20 +1,14 @@
 from pathlib import Path
-from typing import Dict, List, Literal
+from typing import Dict, List, Callable
 
-import pandas as pd
-import modin.pandas as modin_pd
+from pandas import DataFrame
 
 
 def save_to_parquet(
-    dfs: List[Dict[str, pd.DataFrame]], concat_fn_type: Literal['pandas', 'modin'], data_type_key: str, full_file_path: Path
+    dfs: List[Dict[str, DataFrame]], concat_fn: Callable, data_type_key: str, full_file_path: Path
 ):
     wanted_dfs = [df_dict.get(data_type_key) for df_dict in dfs]
     wanted_dfs = [df for df in wanted_dfs if df is not None]
     if len(wanted_dfs) > 0:
-        if concat_fn_type == "pandas":
-            full_df = pd.concat(wanted_dfs, ignore_index=True)
-        elif concat_fn_type == "modin":
-            full_df = modin_pd.concat(wanted_dfs, ignore_index=True)
-        else:
-            raise ValueError(f"Invalid concat_fn_type {concat_fn_type}")
+        full_df = concat_fn(wanted_dfs, ignore_index=True)
         full_df.to_parquet(full_file_path)

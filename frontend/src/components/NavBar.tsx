61c6c0cda8414c798b9ba3fd8779bca61ac76108
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-04-26 10:50:45 -0700

    Add navbar and homepage scaffolding

---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,41 @@
+import { Box, Flex } from "@chakra-ui/react"
+import { Link, useLocation } from "react-router-dom"
+
+const NavBar = () => {
+  const location = useLocation();
+
+  const nav = [
+    {
+      "name": "Dashboard",
+      "href": "/"
+    },
+    {
+      "name": "Queue",
+      "href": "/#"
+    }
+  ];
+
+  return (
+    <Flex gap={8}>
+      <Box fontSize="xx-large" fontWeight={600} marginY="auto">Thunderbird Data Analyzer</Box>
+      <Flex alignItems="center" fontSize="medium" fontWeight={400} gap={4}>
+        {nav.map((link) => (
+          <Link to={link.href}>
+            <Box
+              paddingY={4}
+              paddingX={5}
+              bg={location.pathname === link.href ? "blue.600" : "transparent"}
+              color={location.pathname === link.href ? "white" : " black"}
+              fontWeight={location.pathname === link.href ? "600" : "400"}
+              borderRadius={40}
+            >
+              {link.name}
+            </Box>
+          </Link>
+        ))}
+      </Flex>
+    </Flex>
+  );
+};
+
+export default NavBar
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,33 @@
+import { Box, Flex, Spacer, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
+import NavBar from "../components/NavBar";
 
 function Home() {
 
   return (
-    <>
-      <div>
-        Hello
-      </div>
-    </>
+    <Flex flexDirection="column" gap={10} padding={6}>
+      <NavBar />
+      <Flex>
+      <Tabs>
+          <TabList>
+            <Tab>All</Tab>
+            <Tab>To be converted</Tab>
+            <Tab>To be processed</Tab>
+          </TabList>
+
+          <TabPanels>
+            <TabPanel>
+              <p>One</p>
+            </TabPanel>
+            <TabPanel>
+              <p>Two</p>
+            </TabPanel>
+            <TabPanel>
+              <p>Three</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Flex>
+    </Flex>
   )
 }
 

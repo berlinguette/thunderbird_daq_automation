9e7db25fc36a41652f014fea733aade28180e04e
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-05-16 15:20:36 -0700

    Improve experiment properties maker helper

--- src/automatic_analyzer/tests/conftest.py
+++ src/automatic_analyzer/tests/conftest.py
@@ -3,23 +3,39 @@ Contains common fixtures/helpers that may be used across multiple tests
 """
 
 from pathlib import Path
+from typing import Literal
 from pyfakefs.fake_filesystem import FakeFilesystem
 import pytest
-from automatic_analyzer.experiment_inventory import ExperimentProperties, OverrideInventory
+from automatic_analyzer.experiment_inventory import (
+    ExperimentProperties,
+    OverrideInventory,
+)
 from automatic_analyzer.experiment_tracker import ExperimentTracker
 
-def make_experiment_props():
+
+def make_experiment_props(
+    unc_mtime: float | Literal["Error"] = 0,
+    con_mtime: float | Literal["Error"] = 0,
+    pro_mtime: float | Literal["Error"] = 0,
+    overridden: bool = False,
+):
     return ExperimentProperties(
-        unconverted_mtime=0, converted_mtime=0, processed_mtime=0, overridden=False
+        unconverted_mtime=unc_mtime,
+        converted_mtime=con_mtime,
+        processed_mtime=pro_mtime,
+        overridden=overridden,
     )
+
 
 @pytest.fixture
 def experiment_props():
     return make_experiment_props()
 
+
 @pytest.fixture
 def override_inventory():
     return OverrideInventory(Path("/foo"))
+
 
 @pytest.fixture
 def initialized_fs(fs: FakeFilesystem):
@@ -28,7 +44,10 @@ def initialized_fs(fs: FakeFilesystem):
     fs.create_dir("/pro")
     yield fs
 
+
 @pytest.fixture
 def exp_tracker(override_inventory, initialized_fs):
     override_inventory.set("ID-1..", make_experiment_props())
-    return ExperimentTracker(Path("/unc"), Path("/con"), Path("/pro"), override_inventory)
+    return ExperimentTracker(
+        Path("/unc"), Path("/con"), Path("/pro"), override_inventory
+    )

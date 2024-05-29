00d5b3ea6e20e1add7a0059d6591e43faf9e4b27
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-05-29 11:44:04 -0700

    Add zod validator for frontend log listener

--- frontend/src/types/Log.ts
+++ frontend/src/types/Log.ts
@@ -1,4 +1,48 @@
-export type LogLevels = "NOTSET" | "DEBUG" | "INFO" | "WARNING" | "ERROR" | "CRITICAL";
+import { z } from "zod";
+
+export const LogLevels = z.enum(["NOTSET", "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]);
+
+export type LogLevels = z.infer<typeof LogLevels>;
+
+export const RawLog = z.object({
+  text: z.string(),
+  record: z.object({
+    elapsed: z.object({
+      repr: z.string(),
+      seconds: z.number()
+    }),
+    exception: z.any(),
+    extra: z.record(z.any()),
+    file: z.object({
+      name: z.string(),
+      path: z.string()
+    }),
+    function: z.string(),
+    level: z.object({
+      icon: z.string(),
+      name: LogLevels,
+      no: z.number()
+    }),
+    line: z.number(),
+    message: z.string(),
+    module: z.string(),
+    name: z.string(),
+    process: z.object({
+      id: z.number(),
+      name: z.string(),
+    }),
+    thread: z.object({
+      id: z.number(),
+      name: z.string()
+    }),
+    time: z.object({
+      repr: z.string(),
+      timestamp: z.number()
+    })
+  })
+});
+
+export type RawLog = z.infer<typeof RawLog>;
 
 export type Log = {
   timestamp: string,

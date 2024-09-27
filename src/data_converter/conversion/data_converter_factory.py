e669726d330538e3a8fad48366a8c723cdff8e51
Ryan Oldford <ryan.oldford@gmail.com>
2024-09-26 17:16:26 -0700

    Refactored out complex conditional expression

--- src/data_converter/conversion/data_converter_factory.py
+++ src/data_converter/conversion/data_converter_factory.py
@@ -67,10 +67,8 @@ class DataConverterFactory:
             root_path = self._find_caen_root(
                 source_path.parent, found_subfolder=found_subfolder)
         else:
-            def check_caen_subfolder(folder_name: str) -> bool:
-                return (
-                    found_subfolder == folder_name or
-                    (source_path / folder_name).exists())
+            def check_caen_subfolder(expected_subfolder: str) -> bool:
+                return self._is_expected_subfolder(expected_subfolder, found_subfolder, source_path)
 
             checks: dict[str, bool] = {
                 "Missing CAEN metadata file (exp.info)": (source_path / constants.CAEN_RUN_INFO).exists(),
@@ -115,6 +113,11 @@ class DataConverterFactory:
         return any((
             pattern.fullmatch(file.name) for file in folder_path.iterdir()
         ))
+        
+    def _is_expected_subfolder(self, expected_subfolder: str, subfolder: str | None, parent_folder: Path) -> bool:
+        return (
+            subfolder == expected_subfolder or
+            (parent_folder / expected_subfolder).exists())
 
     def _are_psdata_files_here(self, folder_path: Path) -> bool:
         return self._does_matching_file_exist(
